4b1aecbbc339e5a31c0537060394e6a2b9a453af
Marcelo Massimi <marcelo@massimi.com.ar>
2023-02-14 19:10:58 -0300

    agregue variables al motors test

--- tools/Test-CoreXY-StepperMotors/Motors_test/src/main.cpp
+++ tools/Test-CoreXY-StepperMotors/Motors_test/src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
 
+//  Motor
+const byte MOTOR_WHITE_DIR (2);
+const byte MOTOR_WHITE_STEP (3);
+const byte MOTOR_BLACK_DIR (4);
+const byte MOTOR_BLACK_STEP (5);
+const byte SQUARE_SIZE = 195;
+const int SPEED_SLOW (3000);
+const int SPEED_FAST (1000);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //  MOTOR
+  pinMode (MOTOR_WHITE_STEP, OUTPUT);
+  pinMode (MOTOR_WHITE_DIR, OUTPUT);
+  pinMode (MOTOR_BLACK_STEP, OUTPUT);
+  pinMode (MOTOR_BLACK_DIR, OUTPUT);
+  Serial.println("Setup OK")
 }
 
 void loop() {
